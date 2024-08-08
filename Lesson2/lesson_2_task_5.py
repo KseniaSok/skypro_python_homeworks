2c6dc0feb84990d2769c80f7052b7795223864d6
KseniaSok <xxeshas@gmail.com>
2024-08-08 22:38:14 +0300

    HW2

---
+++ Lesson2/lesson_2_task_5.py
@@ -0,0 +1,25 @@
+month_to_season = float (input('Введите номер месяца: '))
+if month_to_season == 1:
+        print("Зима")
+elif month_to_season == 2:
+         print("Зима")
+elif month_to_season == 12:
+         print("Зима")
+elif month_to_season == 3:
+         print("Весна")
+elif month_to_season == 4:
+         print("Весна")
+elif month_to_season == 5:
+         print("Весна")
+elif month_to_season == 6:
+         print("Лето")
+elif month_to_season == 7:
+         print("Лето")
+elif month_to_season == 8:
+         print("Лето")
+elif month_to_season == 9:
+         print("Осень")
+elif month_to_season == 10:
+         print("Осень")
+elif month_to_season == 11:
+         print("Осень")
---
+++ Lesson2/lesson_2_task_8.py
@@ -0,0 +1,4 @@
+list = list(range(18, 2, -4))
+print(list)
+
+#18, 14, 10, 6, 2
