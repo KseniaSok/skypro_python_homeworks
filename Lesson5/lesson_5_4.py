ccf0cb9093c99ce257ddcd46393874a016ba4685
KseniaSok <xxeshas@gmail.com>
2024-08-27 21:06:42 +0300

    HW5

--- Lesson5/lesson_5_4.py
+++ Lesson5/lesson_5_4.py
@@ -10,5 +10,6 @@ driver.get("http://the-internet.herokuapp.com/entry_ad")
 wait = WebDriverWait(driver, 10)
 modal_window = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal")))
 #В модальном окне нажмите на кнопку Сlose
-close_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".modal-footer .close-button"))).click()
+close_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".modal-footer .close-button")))
+close_button.click()
 driver.quit()
