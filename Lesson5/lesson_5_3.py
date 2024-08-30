a1875a7e91aa4e0c0e43cdcd9a8334027be0aa3d
KseniaSok <xxeshas@gmail.com>
2024-08-30 20:23:41 +0300

    HW5

--- Lesson5/lesson_5_3.py
+++ Lesson5/lesson_5_3.py
@@ -13,5 +13,5 @@ driver = webdriver.Chrome()
 #Запустите скрипт три раза подряд. 
 for i in range(3):
     driver.get("http://uitestingplayground.com/classattr")
-    bluebutton = driver.find_element(By.CSS_SELECTOR, "button[class='btn class3 btn-primary btn-test']").click()
+    bluebutton = driver.find_element(By.CSS_SELECTOR, "button[class='btn-primary']").click()
 driver.quit()
