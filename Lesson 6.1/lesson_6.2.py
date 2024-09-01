172ddbe6120d799da19649ddb0e5a382607a4080
KseniaSok <xxeshas@gmail.com>
2024-09-01 13:01:17 +0300

    HW6.1

--- Lesson 6.1/lesson_6.2.py
+++ Lesson 6.1/lesson_6.2.py
@@ -8,18 +8,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
+def test():
+    driver = webdriver.Chrome()
+    driver.implicitly_wait(60)
+    driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
+    driver.find_element(By.CSS_SELECTOR, '#delay').clear()
+    driver.find_element(By.CSS_SELECTOR, '#delay').send_keys('45')
 
-driver = webdriver.Chrome()
-driver.implicitly_wait(60)
-driver.get("https://bonigarcia.dev/selenium-webdriver-java/slow-calculator.html")
-driver.find_element(By.CSS_SELECTOR, '#delay').clear()
-sleep(5)
-driver.find_element(By.CSS_SELECTOR, '#delay').send_keys('45')
-sleep(5)
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[1]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[4]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]').click()
-driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]').click()
-sleep(50)
-waiter = WebDriverWait(driver, 60, 1)
-#checkout_summary_container > div > div.summary_info > div.summary_total_labeldriver.quit()
+    driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[1]').click()
+    driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[4]').click()
+    driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[2]').click()
+    driver.find_element(By.XPATH, '//*[@id="calculator"]/div[2]/span[15]').click()
+    waiter = WebDriverWait(driver, 60, 1)
+    assert "15" in driver.find_element(By.XPATH, '//*[@id="calculator"]/div[1]/div').text
+    driver.quit()
--- Lesson 6.1/lesson_6.3.py
+++ Lesson 6.1/lesson_6.3.py
@@ -9,41 +9,23 @@ from selenium.webdriver.support import expected_conditions as EC
 import pytest
 
 
-driver = webdriver.Chrome()
-driver.get('https://www.saucedemo.com/')
-driver.find_element(By.CSS_SELECTOR, '#user-name').send_keys('standard_user')
-driver.find_element(By.CSS_SELECTOR, '#password').send_keys('secret_sauce')
-driver.find_element(By.CSS_SELECTOR, '#login-button').click()
-driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack').click()
-driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt').click()
-driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie').click()
-driver.find_element(By.CSS_SELECTOR, '#shopping_cart_container').click()
-driver.find_element(By.CSS_SELECTOR, '#checkout').click()
-driver.find_element(By.CSS_SELECTOR, '#first-name').send_keys('Alex')
-driver.find_element(By.CSS_SELECTOR, '#last-name').send_keys('Fox')
-driver.find_element(By.CSS_SELECTOR, '#postal-code').send_keys('123456')
-driver.find_element(By.CSS_SELECTOR, '#continue').click()
+def test():
+    driver = webdriver.Chrome()
+    driver.get('https://www.saucedemo.com/')
+    driver.find_element(By.CSS_SELECTOR, '#user-name').send_keys('standard_user')
+    driver.find_element(By.CSS_SELECTOR, '#password').send_keys('secret_sauce')
+    driver.find_element(By.CSS_SELECTOR, '#login-button').click()
+    driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-backpack').click()
+    driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-bolt-t-shirt').click()
+    driver.find_element(By.CSS_SELECTOR, '#add-to-cart-sauce-labs-onesie').click()
+    driver.find_element(By.CSS_SELECTOR, '#shopping_cart_container').click()
+    driver.find_element(By.CSS_SELECTOR, '#checkout').click()
+    driver.find_element(By.CSS_SELECTOR, '#first-name').send_keys('Alex')
+    driver.find_element(By.CSS_SELECTOR, '#last-name').send_keys('Fox')
+    driver.find_element(By.CSS_SELECTOR, '#postal-code').send_keys('123456')
+    driver.find_element(By.CSS_SELECTOR, '#continue').click()
 
-total_label = driver.find_element(By.CSS_SELECTOR, '[data-test=total-label]').text
+    total_label = driver.find_element(By.CSS_SELECTOR, '[data-test=total-label]').text
+    assert total_label == ('Total: $58.29')
 
-assert total_label == 'Total: $58.29'
-
-driver.quit()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-driver.quit()
+    driver.quit()
