84443272d91cff02fc7d3067cd5d892fcff67de0
KseniaSok <xxeshas@gmail.com>
2024-09-29 13:22:44 +0300

    HW8

--- Lesson8/Token.py
+++
@@ -1,9 +0,0 @@
-import requests
-import pytest
-from Lesson8.URL import base_url
-
-def get_token(username='flora', password='nature-fairy'):
-    log_password = {'username' : username, 'password' : password}
-    resp_token = requests.post(base_url + '/auth/login', json=log_password)
-    token = resp_token.json()['userToken']
-    return token
